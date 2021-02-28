c6b645eaf000697092c9505e28a65aadf05ab5a3
RiajulKashem <riajul.programmer@gmail.com>
2021-02-28 18:39:03 +0600

    jazzmin addded

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',  # https://github.com/farridav/django-jazzmin
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
